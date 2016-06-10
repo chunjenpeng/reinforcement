1b620de48721ebcf6f0f32034d9240743bbfc951
DanielLoney <danieljloneyacc@gmail.com>
2016-06-10 13:26:13 +0800

    Revert "minor changes"
    This reverts commit 7734b551938790f87603d77a31714bcbb60fb006.

--- interviewer.py
+++ interviewer.py
@@ -1,41 +1,53 @@
 #author Daniel & Ryan
-import util, layout
+import util, sys
+from layout import Layout
 from pacman import GameState
 from game import GameStateData
 from game import Game
 from game import Directions
 from game import Actions
+from random import randint
+from random import seed
 
-mazeHeight = 1
-mazeLength = 5
-posPacman = 1
-posGhost = 4
 listFood = []
 listCapsule = []
-gameData = dict(mazeLength = mazeLength, posPacman = posPacman, 
-            posGhost = posGhost, listFood = listFood, listCapsule = listCapsule)
+seed = 0
 
 usedGameStates = []
-
-def generateGameData(seed = 0):
-    gameData = [mazeLength, posPacman, posGhost, listFood, listCapsule]
-    return gameData
     
 def ghostRule(gameData):
     return True
 
-def generateLayout():
-    layoutText = [None]*(2+mazeHeight)
+def generateLayout(gameData):
+    listFood = []
+    listCapsule = []
+    height = gameData['mazeHeight']
+    length = gameData['mazeLength']
+    posPacman = gameData['posPacman']
+    posGhost = gameData['posGhost']
+    layoutText = [None]*(2+height)
 
-    wall = "%"*(mazeLength+2)
+    wall = "%"*(length+2)
 
     layoutText[0] = wall
-    layoutText[mazeHeight+1] = wall
+    layoutText[height+1] = wall
 
-    for x in range(1,(mazeHeight+1)):
+    for k in range(1,length+1): #randomization of food and capsules
+        if k == posPacman:
+            continue
+        if k == posGhost:
+            continue
+        #random.seed(0)
+        randomInt = randint(0,2)
+        if randomInt == 1:
+            listCapsule.append(k)
+        elif randomInt == 2:
+            listFood.append(k)
+
+    for x in range(1,(height+1)):
         row = "%"
 
-        for k in range(1,(mazeLength+1)):
+        for k in range(1,(length+1)):
             if k == posPacman:
                 row += "P"
             elif k == posGhost:
@@ -46,36 +58,75 @@ def generateLayout():
                 row += "o"
             else:
                 row += " "
+
         row += "%"
         layoutText[x] = row
 
-    return layoutText
+    return Layout(layoutText)
 
 def generateGameState(gameData): 
     #gamestate = GameState(generateLayout())
     #layout = generateLayout(gameData)
-    layoutName = 'test1D' 
-    Layout = layout.getLayout(layoutName) 
+    #layoutName = 'test1D' 
+    layout =  generateLayout(gameData)
     gameState = GameState()
     numGhostAgents = 1
-    gameState.initialize(Layout, numGhostAgents)
+    gameState.initialize(layout, numGhostAgents)
     print gameState
     return gameState
     
-def generateGameStates():
+def generateGameStates(gameData):
+    listGameStates = []
     randomSeed = 0
-    for repeat in range (0, 10, 1):
-        gameData = generateGameData(randomSeed)
+    for repeat in range (0, gameData['numLayouts']):
         if ghostRule(gameData):
             gameState = generateGameState(gameData)
             if gameState not in usedGameStates:
-                print gameState
+                listGameStates.append(gameState)
+                usedGameStates.append(gameState)
+    return listGameStates
 
 def getAction(gameState):
     import pacmanAgents, qlearningAgents
     pacmanAgent = pacmanAgents.GreedyAgent()
     action = pacmanAgent.getAction(gameState)
-    return action 
+    return action
 
-print 'Pacman Action: '+getAction(generateGameState(gameData))
-#generateGameStates()
+def default(str):
+  return str + ' [Default: %default]'
+
+def readCommand(argv):
+
+    from optparse import OptionParser
+
+    usageStr = ""
+    parser = OptionParser(usageStr)
+
+    parser.add_option('--mazeLength', dest = 'mazeLength', type='int',
+                      help = default('the length of the maze'), default = 5)
+    parser.add_option('--mazeHeight', dest = 'mazeHeight', type='int',
+                      help = default('the height of the maze'), default = 1)
+    parser.add_option('--posPacman', dest = 'posPacman', type='int',
+                      help = default('the position of pacman in a horizontal maze'), default = 1)
+    parser.add_option('--posGhost', dest = 'posGhost', type='int',
+                      help = default('the position of the ghost in a horizontal maze'), default = 4)
+    parser.add_option('--numLayouts' ,dest = 'numLayouts', type='int',
+                      help = default('the number of layouts to be generated'), default = 10 )
+
+    options, otherjunk = parser.parse_args(argv)
+
+    if len(otherjunk) != 0:
+        raise Exception('Command line input not understood: ' + str(otherjunk))
+
+    args = dict()
+
+    args['mazeLength'] = options.mazeLength
+    args['mazeHeight'] = options.mazeHeight
+    args['posPacman'] = options.posPacman
+    args['posGhost'] = options.posGhost
+    args['numLayouts'] = options.numLayouts
+
+    return args
+args = readCommand( sys.argv[1:] )
+for k in range(0,args['numLayouts']):
+    print 'Pacman Action: ' + getAction(generateGameState(args)) + "\n"
